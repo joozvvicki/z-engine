3e98bf9ba9a433c9bff8bde2ee8e51b2184f8a47
Dominik Jóźwicki <joozvvicki@gmail.com>
2026-02-01 19:40:37 +0100

    Improved ZLogger

--- src/engine/utils/ZLogger.ts
+++ src/engine/utils/ZLogger.ts
@@ -3,53 +3,50 @@ class ZLogger {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   private isDebugging: boolean = (import.meta as any).env?.DEV
 
+  public log: (...messages: unknown[]) => void
+  public error: (...messages: unknown[]) => void
+  public warn: (...messages: unknown[]) => void
+  public info: (...messages: unknown[]) => void
+  public debug: (...messages: unknown[]) => void
+
   constructor(tag: string) {
     this.tag = tag
-  }
 
-  log(...messages: unknown[]): void {
-    if (!this.isDebugging) return
-    console.log(
-      `%c${this.tag}`,
-      'background-color: #00ffaa; border-radius: 4px; padding: 2px 4px; color: #000',
-      ...messages
-    )
-  }
+    const noop = (): void => {}
 
-  error(...messages: unknown[]): void {
-    if (!this.isDebugging) return
-    console.error(
-      `%c${this.tag}`,
-      'background-color: #ad0000; border-radius: 4px; padding: 2px 4px; color: #fff',
-      ...messages
-    )
-  }
-
-  warn(...messages: unknown[]): void {
-    if (!this.isDebugging) return
-    console.warn(
-      `%c${this.tag}`,
-      'background-color: #ffcc00; border-radius: 4px; padding: 2px 4px; color: #000',
-      ...messages
-    )
-  }
-
-  info(...messages: unknown[]): void {
-    if (!this.isDebugging) return
-    console.info(
-      `%c${this.tag}`,
-      'background-color: #00aaff; border-radius: 4px; padding: 2px 4px; color: #fff',
-      ...messages
-    )
-  }
-
-  debug(...messages: unknown[]): void {
-    if (!this.isDebugging) return
-    console.debug(
-      `%c${this.tag}`,
-      'background-color: #00aaff; border-radius: 4px; padding: 2px 4px; color: #fff',
-      ...messages
-    )
+    if (this.isDebugging) {
+      this.log = console.log.bind(
+        console,
+        `%c${this.tag}`,
+        'background-color: #00ffaa; border-radius: 4px; padding: 2px 4px; color: #000'
+      )
+      this.error = console.error.bind(
+        console,
+        `%c${this.tag}`,
+        'background-color: #ad0000; border-radius: 4px; padding: 2px 4px; color: #fff'
+      )
+      this.warn = console.warn.bind(
+        console,
+        `%c${this.tag}`,
+        'background-color: #ffcc00; border-radius: 4px; padding: 2px 4px; color: #000'
+      )
+      this.info = console.info.bind(
+        console,
+        `%c${this.tag}`,
+        'background-color: #00aaff; border-radius: 4px; padding: 2px 4px; color: #fff'
+      )
+      this.debug = console.debug.bind(
+        console,
+        `%c${this.tag}`,
+        'background-color: #00aaff; border-radius: 4px; padding: 2px 4px; color: #fff'
+      )
+    } else {
+      this.log = noop
+      this.error = noop
+      this.warn = noop
+      this.info = noop
+      this.debug = noop
+    }
   }
 
   with(tag: string): ZLogger {
